1a0af186b041ef6a4e7f9ef546aea0f9cad09c96
surya <techeducation110@gmail.com>
2021-08-05 13:16:52 +0530

    Commit first

---
+++ src/test/java/com/commonUtilis/DEMO.java
@@ -0,0 +1,40 @@
+package com.commonUtilis;
+
+public class DEMO {
+
+
+		  public void fullThrottle() {
+		    System.out.println("The car is going as fast as it can!");
+		  }
+
+		  public void speed(int maxSpeed) {
+		    System.out.println("Max speed is: " + maxSpeed);
+		  }
+
+		  public static void main(String[] args) {
+			  
+			  DEMO demo = new DEMO();
+			  demo.fullThrottle();
+			  demo.speed(200);
+			  
+//		    Main myCar = new Main();   // Create a myCar object
+//		    myCar.fullThrottle();      // Call the fullThrottle() method
+//		    myCar.speed(200);          // Call the speed() method
+		  }
+		}
+
+		// The car is going as fast as it can!
+		// Max speed is: 200
+
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+		
+	
